3b598e93175c37aab00d0032f90dbd7459be5c3b
Mihail <mihailmilkov@gmail.com>
2014-01-08 01:39:59 +0200

    Update excelreader.go

---
+++ excelreader.go
@@ -0,0 +1,28 @@
+package ExcelReader
+
+type SimpleExcellReader struct {
+    var cellValues[][] string
+    var collsLen int
+    var rowsLen int
+    type OLE interface{}
+}
+
+func (ser SimpleExcellReader) Open(filePath string , opt array) SimpleExcellReader {
+
+}
+
+func (ser SimpleExcellReader) Value(i,j int) stirng {
+
+}
+func (ser SimpleExcellReader) RowLen() int {
+
+}
+func (ser SimpleExcellReader) CollLen(coll int) int {
+
+}
+func (ser SimpleExcellReader) Colls() []string {
+
+}
+func (ser SimpleExcellReader) Rows() []string {
+
+}
